fc5d0b342801fde59c963c69f9e3ec097671ecbe
caiopapai <caio_papai@hotmail.com>
2020-05-03 19:51:42 -0300

    stream examples

--- src/br/com/moder/java/in/action/part2/streams/streams/introduction/MainAppStream.java
+++ src/br/com/moder/java/in/action/part2/streams/streams/introduction/MainAppStream.java
@@ -2,21 +2,22 @@ package br.com.moder.java.in.action.part2.streams.streams.introduction;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MainAppStream {
 
     public static void main(String[] args) {
 
-        List<Dish> menu = Arrays.asList(
-                new Dish("pork",false,800,DishType.MEAT),
-                new Dish("beef",false,700,DishType.MEAT),
-                new Dish("chicken", false, 400, DishType.MEAT),
-                new Dish("french fires", true, 530, DishType.OTHER),
-                new Dish("rice", true,350, DishType.OTHER),
-                new Dish("season fruit", true,120,DishType.OTHER),
-                new Dish("pizza", true, 550, DishType.OTHER),
-                new Dish("prawns", false, 300, DishType.FISH),
-                new Dish("salmon", false, 450, DishType.FISH)
-        );
+        DishesRepository repository = new DishesRepository();
+
+        List<Dish> menu = repository.getllDishes();
+
+        List<String> threeHighCaloricDishNames = menu.stream() // Creates a pipeline
+                .filter(dish -> dish.getCalories() > 300) //Filter high-calorie dishes
+                .map(Dish::getName) // Get the names of Dishes
+                .limit(3) // Select only the first three
+                .collect(Collectors.toList()); //Stores the result in another list;
+
+        System.out.println(threeHighCaloricDishNames);
     }
 }
